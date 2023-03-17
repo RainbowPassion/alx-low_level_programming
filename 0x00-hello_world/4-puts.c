5b25600dabeb1bca57c73af9c4db42f089b442fa
Madonna <madonna.dee.md@gmail.com>
2023-03-17 16:41:15 -0700

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - Prints what is required followed by a line
+*
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\r\n");
+return (0);
+}
