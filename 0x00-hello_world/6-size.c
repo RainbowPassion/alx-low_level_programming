8588149e1192676fcc5923e3f1c08c6908ff8c20
Madonna <madonna.dee.md@gmail.com>
2023-03-17 18:08:59 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int intType;
 long int longType;
 long long int longlongType;
 float floatType;
-printf("Size of char: %ld byte(s)\n", sizeof(charType));
-printf("Size of int: %ld bytes(s)\n", sizeof(intType));
-printf("Size of long int: %ld bytes(s)\n", sizeof(longType));
-printf("Size of long long int: %ld bytes(s)\n", sizeof(longlongType));
-printf("Size of float: %ld bytes(s)\n", sizeof(floatType));
+printf("Size of char: %lu byte(s)\n", sizeof(charType));
+printf("Size of int: %lu bytes(s)\n", sizeof(intType));
+printf("Size of long int: %lu bytes(s)\n", sizeof(longType));
+printf("Size of long long int: %lu bytes(s)\n", sizeof(longlongType));
+printf("Size of float: %lu bytes(s)\n", sizeof(floatType));
 return (0);
 }
