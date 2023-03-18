acc08100c50e92f8e8f1c59b07e7289b281a4a24
Madonna <madonna.dee.md@gmail.com>
2023-03-17 19:24:30 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Prints to the standard error
+*
+*
+* Return: 1 (Success)
+*/
+int main(void)
+{
+fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stderr);
+return (1);
+}
