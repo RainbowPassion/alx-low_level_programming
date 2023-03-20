7bb04bbd26c0cc9965a2862504357c09fd731b09
Madonna <madonna.dee.md@gmail.com>
2023-03-20 15:51:06 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,10 +10,11 @@
 int main(void)
 {
 int n;
+int last_digit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+last_digit = n % 10;
 printf("Last digit of %d is ", n);
-int last_digit = n % 10;
 if (last_digit > 5)
 {
 printf("%d and is greater than 5\n", last_digit);
