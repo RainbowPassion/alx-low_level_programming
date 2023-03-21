827e685de9a167081ca1b7469e1a0186042a584f
Madonna <madonna.dee.md@gmail.com>
2023-03-20 17:45:18 -0700

    print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main -     prints all possible different combinations of two digits.
+*
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int n, j;
+for (n = 0; n < 10; n++)
+{
+for (j = n + 1; j < 10; j++)
+{
+putchar(n + '0');
+putchar(j + '0');
+putchar(',');
+putchar(' ');
+}
+}
+return (0);
+}
