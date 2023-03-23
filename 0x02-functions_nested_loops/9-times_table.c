8e0a59cea9ce9ee6ad9f08133872c5f06ecb5643
Madonna <madonna.dee.md@gmail.com>
2023-03-22 17:42:16 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,24 +6,24 @@
 */
 void times_table(void)
 {
-int i, j;
+int i, j, result;
 for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 9; j++)
 {
-int result = i * j;
+result = i * j;
 if (result > 9)
 {
-_putchar((result / 10) + '0');
-_putchar((result % 10) + '0');
-_putchar(',');
-_putchar(' ');
+_putchar(result / 10 + '0');
 }
 else
 {
-_putchar(result + '0');
-_putchar(',');
 _putchar(' ');
+}
+_putchar(result % 10 + '0');
+if (j < 9)
+{
+_putchar(',');
 _putchar(' ');
 }
 }
