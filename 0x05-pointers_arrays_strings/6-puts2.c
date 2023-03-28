548bba110c2d6fb8e3dd9d78dbde0321ea6ebcce
Madonna <madonna.dee.md@gmail.com>
2023-03-28 13:45:08 -0700

    Half the lies they tell about me arent true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* puts2 - prints every other character of a string
+* @str: input
+* Return: print
+*/
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+	putchar(str[i]);
+	}
+	putchar('\n');
+}
