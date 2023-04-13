6c8a32c6e5883269115d378d185fd152ca6261d9
Madonna <madonna.dee.md@gmail.com>
2023-04-13 14:58:33 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+* str_concat -  returns a pointer to a 2 dimensional array of integers.
+* @s1: pointer
+* @s2: pointer
+* Return: results
+*/
+char *str_concat(char *s1, char *s2)
+{
+int i;
+int s1_len =  0;
+int s2_len =  0;
+int result_len;
+char *result;
+if ((s1 == NULL) || (s2 == NULL))
+return (NULL);
+s1_len =  strlen(s1);
+s2_len = strlen(s2);
+result_len = s1_len + s2_len;
+result = malloc(result_len *sizeof(char));
+if (result == NULL)
+return (NULL);
+for (i = 0; i < s1_len; i++)
+{
+result[i] = s1[i];
+}
+for (i = 0; i < s2_len; i++)
+{
+result[s1_len + i] = s2[i];
+}
+result[result_len] = '\0';
+return (result);
+}
