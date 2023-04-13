6486e3ae27923e29b8da8e262ea90c04354cd2ae
Madonna <madonna.dee.md@gmail.com>
2023-04-13 15:23:22 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,9 +14,9 @@ int s1_len =  0;
 int s2_len =  0;
 int result_len;
 char *result;
-if ((s1 == NULL) || (s2 == NULL))
-return (NULL);
+if (s1 != NULL)
 s1_len =  strlen(s1);
+if(s2 != NULL)
 s2_len = strlen(s2);
 result_len = s1_len + s2_len + 1;
 result = malloc(result_len *sizeof(char));
