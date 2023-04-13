d4f29b091456865a9459943cd4f0774897f0a4de
Madonna <madonna.dee.md@gmail.com>
2023-04-13 15:03:54 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,7 +18,7 @@ if ((s1 == NULL) || (s2 == NULL))
 return (NULL);
 s1_len =  strlen(s1);
 s2_len = strlen(s2);
-result_len = s1_len + s2_len;
+result_len = s1_len + s2_len + 1;
 result = malloc(result_len *sizeof(char));
 if (result == NULL)
 return (NULL);
@@ -30,6 +30,6 @@ for (i = 0; i < s2_len; i++)
 {
 result[s1_len + i] = s2[i];
 }
-result[result_len] = '\0';
+result[result_len - 1] = '\0';
 return (result);
 }
